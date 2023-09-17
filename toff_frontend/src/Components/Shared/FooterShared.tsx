b2934e44a3af0d58cf6f94721ab7a877fcd8dab4
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-09-17 23:51:58 +0200

    Added FooterShared.tsx

---
+++ toff_frontend/src/Components/Shared/FooterShared.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function FooterShared() {
+    return(
+      <div>
+          <a href="#impressum">Impressum</a>
+      </div>
+    );
+}
+
+export default FooterShared;
