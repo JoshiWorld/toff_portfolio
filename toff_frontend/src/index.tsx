4c10c5ade61bd42642dfa2e51a6d5e764b8a012c
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-09-17 18:19:21 +0200

    Installed react-bootstrap

--- toff_frontend/src/index.tsx
+++ toff_frontend/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SSRProvider from 'react-bootstrap/SSRProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <SSRProvider>
+          <App />
+      </SSRProvider>
   </React.StrictMode>
 );
 
