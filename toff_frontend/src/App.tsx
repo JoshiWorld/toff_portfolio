44552a39d6eb7365fed82c9e369371297582dd80
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-09-17 23:52:31 +0200

    Added image on home

--- toff_frontend/src/App.tsx
+++ toff_frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import NavbarShared from './Components/Shared/NavbarShared';
+import Home from './Components/Home';
+import FooterShared from './Components/Shared/FooterShared';
 
 function App() {
   return (
     <div className="App">
       <NavbarShared />
+        <Home />
+        <FooterShared />
     </div>
   );
 }
--- toff_frontend/src/Components/Home.tsx
+++ toff_frontend/src/Components/Home.tsx
@@ -1,7 +1,37 @@
-import * as React from 'react';
+import React from 'react';
+import homeImage from '../Images/home.png';
 
 function Home() {
+    const imageContainerStyle: React.CSSProperties = {
+        maxWidth: '100%',
+        height: 'auto',
+        position: 'relative',
+    };
 
+    const gradientOverlayStyle: React.CSSProperties = {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: '10%', // Adjust the height to control the fading area
+        background: 'linear-gradient(transparent, white)', // Create a gradient from transparent to white
+        pointerEvents: 'none', // Allow clicks to pass through the overlay
+    };
+
+    const imageStyle: React.CSSProperties = {
+        maxWidth: '100%',
+        maxHeight: '100%',
+        width: 'auto',
+        height: 'auto',
+        zIndex: 1, // Ensure the image is above the gradient overlay
+    };
+
+    return (
+        <div style={imageContainerStyle}>
+            <div style={gradientOverlayStyle}></div>
+            <img src={homeImage} alt="TOFF Home Picture" style={imageStyle} />
+        </div>
+    );
 }
 
 export default Home;
