0c0833e1d7ca203a0d4002f145f35cc7ce2d459e
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-09-18 17:47:41 +0200

    updated App.tsx Routes

--- toff_frontend/src/App.tsx
+++ toff_frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Home from './Components/Home';
 import FooterShared from './Components/Shared/FooterShared';
 import Musik from './Components/Musik';
 import NoMatch from './Components/Shared/NoMatch';
+import Kontakt from './Components/Kontakt';
+import Live from './Components/Live';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/musik" element={<Musik />} />
+              <Route path="/kontakt" element={<Kontakt />} />
+              <Route path="/live" element={<Live />} />
               <Route path="*" element={<NoMatch />} />
           </Routes>
           <FooterShared />
