7039396cd3e9b61b9f213cf4f9aedf97b9a858bd
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-09-18 14:53:09 +0200

    added 404 not found page

---
+++ toff_frontend/src/Components/Shared/NoMatch.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function NoMatch() {
+    return (
+        <div style={{ padding: 20 }}>
+            <h2>404: Page Not Found</h2>
+            <p>Diese Seite existiert nicht.</p>
+        </div>
+    );
+}
+
+export default NoMatch;
