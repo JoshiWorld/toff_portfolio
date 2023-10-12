ad33693e7ade88ce962214209fadd849a552bc1f
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-13 01:00:52 +0200

    updated App Base URL

--- toff_frontend/src/Components/Admin/AdminLiveAuftritte.tsx
+++ toff_frontend/src/Components/Admin/AdminLiveAuftritte.tsx
@@ -165,7 +165,7 @@ function AdminLiveAuftritte() {
                                 />
                             ) : item.imageSource ? (
                                 <img
-                                    src={`http://localhost:3030/${item.imageSource}`}
+                                    src={`${API_BASE_URL}/${item.imageSource}`}
                                     alt="Source"
                                     style={{ maxWidth: '30%', maxHeight: '30%', width: '30', height: 'auto' }}
                                 />
