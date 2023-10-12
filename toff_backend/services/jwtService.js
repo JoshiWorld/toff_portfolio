83609497d9f8fc644aa3cd34c140e9b39f0c1c36
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 14:10:19 +0200

    added jwt service for verifying tokens

---
+++ toff_backend/services/jwtService.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+export function verifyToken(req, res, next) {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
+        if (error) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        next();
+    });
+}
