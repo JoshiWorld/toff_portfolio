dd64d878432ab880e43fc30334c78c4a09f2007c
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 02:15:08 +0200

    fixed image sending live

--- toff_backend/routes/live.js
+++ toff_backend/routes/live.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 const mysqlService = require('../services/mysqlService');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const { Blob } = require('blob');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Specify the directory where uploaded files will be saved
+    },
+    filename: function (req, file, cb) {
+        // Use the current timestamp as a unique file name
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -54,23 +70,40 @@ router.delete('/:id', function(req, res) {
     });
 });
 
-router.put('/:id', function(req, res) {
-   const token = req.body.token;
+router.put('/:id', verifyToken,  upload.single('image'), function (req, res) {
+    const updatedItem = JSON.parse(req.body.item);
 
-    jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
-        if(error) {
-            res.status(401).json({ message: 'Unauthorized' });
-        } else {
-            mysqlService.updateLiveBlog(req.params.id, (error, results) => {
-                if(error) {
-                    res.status(500).json({ message: 'Internal server error', error: error });
-                    return;
-                }
+    if (req.file) {
+        const imageSource = req.file.path;
 
-                res.json(results);
-            });
+        updatedItem.imageSource = imageSource;
+    }
+
+    mysqlService.updateLiveBlog(req.params.id, updatedItem, (error, results) => {
+        if (error) {
+            res.status(500).json({ message: 'Internal server error', error: error });
+            return;
         }
+
+        res.json(results);
     });
 });
+
+function verifyToken(req, res, next) {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
+        if (error) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        next();
+    });
+}
+
 
 module.exports = router;
