39e5fef7777bf73c93c7e89fc93cc37bc56a5c82
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 02:16:36 +0200

    fixed AdminLogin.tsx

---
+++ toff_frontend/src/Components/Admin/AdminLogin.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import { useAuth } from '../../Utils/AuthProvider';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+
+function AdminLogin() {
+    const { login } = useAuth();
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate(); // Initialize the useNavigate hook
+
+    // @ts-ignore
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3030/api/master/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ password }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const accessToken = data.token;
+                setCookie('jwt', accessToken, 30);
+                login();
+
+                // Redirect to the /admin page
+                navigate('/admin');
+            } else {
+                console.error('Login failed');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '50vh' }}>
+            <Card>
+                <Card.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Button variant="dark" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+function setCookie(name: string, value: string, days: number) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // Calculate the expiration time
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+export default AdminLogin;
