7be3566a76f39f884e9080718d0fdb57d55a0dbf
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 16:02:30 +0200

    Added Goals

--- toff_frontend/src/Components/Admin/AdminStats.tsx
+++ toff_frontend/src/Components/Admin/AdminStats.tsx
@@ -1,9 +1,169 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StatsItem } from '../../Types/types';
+import Container from 'react-bootstrap/Container';
+import { Table, Button } from 'react-bootstrap';
+import { useAuth } from '../../Utils/AuthProvider';
+import CreateStats from './Components/CreateStats';
 
 function AdminStats() {
+    const [stats, setStats] = useState<StatsItem[]>([]);
+    const [editableRow, setEditableRow] = useState<number | null>(null);
+    const [isCreateStatsVisible, setCreateStatsVisible] = useState(false);
+    const { token } = useAuth();
+
+    useEffect(() => {
+        if (stats.length === 0) {
+            // Fetch data from the backend when the component mounts
+            fetch('http://localhost:3030/api/stats') // Replace with your actual API endpoint
+                .then((response) => response.json())
+                .then((data) => setStats(data))
+                .catch((error) => console.error('Error fetching data:', error));
+        }
+    }, []);
+
+    const handleEditClick = (index: number) => {
+        setEditableRow(index);
+    };
+
+    const handleSaveClick = (index: number, item: StatsItem) => {
+        const updatedStats = [...stats];
+        const updatedItem = { ...updatedStats[index] };
+
+        fetch(`http://localhost:3030/api/stats/${updatedItem.id}`, {
+            method: 'PUT',
+            // @ts-ignore
+            headers: {
+                'authorization': token,
+            },
+            body: (() => {
+                const formData = new FormData();
+                formData.append('item', JSON.stringify(updatedItem));
+                return formData;
+            })(),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    // Handle success
+                    // You may want to update the state or perform other actions
+                } else {
+                    // Handle errors
+                }
+            })
+            .catch((error) => {
+                console.error('Error sending data to the backend:', error);
+            });
+
+        // Update the state if needed
+        updatedStats[index] = updatedItem;
+        setStats(updatedStats);
+        setEditableRow(null);
+    };
+
+
+    const handleCancelClick = () => {
+        setEditableRow(null);
+    };
+
+
+    const isRowEditable = (index: number) => index === editableRow;
+
     return (
-        <p>Admin stats edit</p>
-    )
+        <Container>
+            <Table responsive="xl">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Titel</th>
+                    <th>Value</th>
+                    <th>Ziel</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {stats.map((item, index) => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>
+                            {isRowEditable(index) ? (
+                                <input
+                                    type="text"
+                                    value={item.title}
+                                    onChange={(e) => {
+                                        const updatedStats = [...stats];
+                                        updatedStats[index] = {
+                                            ...updatedStats[index],
+                                            title: e.target.value,
+                                        };
+                                        setStats(updatedStats);
+                                    }}
+                                />
+                            ) : (
+                                item.title
+                            )}
+                        </td>
+                        <td>
+                            {isRowEditable(index) ? (
+                                <input
+                                    type="text"
+                                    value={item.value}
+                                    onChange={(e) => {
+                                        const updatedStats = [...stats];
+                                        updatedStats[index] = {
+                                            ...updatedStats[index],
+                                            value: +e.target.value,
+                                        };
+                                        setStats(updatedStats);
+                                    }}
+                                />
+                            ) : (
+                                item.value
+                            )}
+                        </td>
+                        <td>
+                            {isRowEditable(index) ? (
+                                <input
+                                    type="text"
+                                    value={item.goal}
+                                    onChange={(e) => {
+                                        const updatedStats = [...stats];
+                                        updatedStats[index] = {
+                                            ...updatedStats[index],
+                                            goal: +e.target.value,
+                                        };
+                                        setStats(updatedStats);
+                                    }}
+                                />
+                            ) : (
+                                item.goal
+                            )}
+                        </td>
+
+                        <td>
+                            {isRowEditable(index) ? (
+                                <div>
+                                    <Button variant="success" onClick={() => handleSaveClick(index, item)}>Save</Button>
+                                    <Button variant="danger" className="ms-1" onClick={handleCancelClick}>Cancel</Button>
+                                </div>
+                            ) : (
+                                <Button variant="dark" onClick={() => handleEditClick(index)}>Edit</Button>
+                            )}
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+
+            {isCreateStatsVisible ? (
+                <CreateStats show={isCreateStatsVisible} onHide={() => setCreateStatsVisible(false)} />
+            ) : (
+                <>
+                    {/* Your existing code */}
+                    {/* ... */}
+                    <Button variant="success" onClick={() => setCreateStatsVisible(true)}>Erstellen</Button>
+                </>
+            )}
+        </Container>
+    );
 }
 
 export default AdminStats;
