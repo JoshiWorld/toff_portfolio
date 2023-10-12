349e54778b2113d138c36370393ec234aaec2633
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 02:15:50 +0200

    fixed card showing wrong tabs

--- toff_frontend/src/Components/Admin/AdminHome.tsx
+++ toff_frontend/src/Components/Admin/AdminHome.tsx
@@ -1,8 +1,39 @@
-import * as React from 'react';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { useLocation, Link } from 'react-router-dom';
+
+import AdminStats from './AdminStats';
+import AdminLiveAuftritte from './AdminLiveAuftritte';
 
 function AdminHome() {
-    return(
-        <p>Admin-Dashboard</p>
+    const location = useLocation();
+
+    return (
+        <div>
+            <div className="d-flex justify-content-center align-items-center" style={{ height: '10vh' }}>
+                <Nav variant="tabs">
+                    <Nav.Item>
+                        <Link
+                            to="/admin/stats"
+                            className={`nav-link ${location.pathname === '/admin/stats' ? 'active' : ''}`}
+                        >
+                            Stats
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Link
+                            to="/admin/live"
+                            className={`nav-link ${location.pathname === '/admin/live' ? 'active' : ''}`}
+                        >
+                            Live-Auftritte
+                        </Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
+
+            {location.pathname === '/admin/stats' && <AdminStats />}
+            {location.pathname === '/admin/live' && <AdminLiveAuftritte />}
+        </div>
     );
 }
 
