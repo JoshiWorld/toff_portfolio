bb6e523889c69d56392f9d4b8ee0ef2ec18af10f
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 18:45:28 +0200

    finished CreateStats.tsx

---
+++ toff_frontend/src/Components/Admin/Components/CreateStats.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import { InputGroup } from 'react-bootstrap';
+import { useAuth } from '../../../Utils/AuthProvider';
+
+// @ts-ignore
+function CreateStats({ show, onHide }) {
+    const [formData, setFormData] = useState({
+        title: '',
+        value: '',
+        goal: '',
+    });
+    const {token} = useAuth();
+
+    // @ts-ignore
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleAddStats = () => {
+        // Convert formData to a JSON string
+        const formDataJSON = JSON.stringify({ stats: formData });
+
+        fetch('http://localhost:3030/api/stats/create', {
+            method: 'POST',
+            // @ts-ignore
+            headers: {
+                'Content-Type': 'application/json', // Set the content type
+                'authorization': token,
+            },
+            body: formDataJSON, // Use the JSON string as the request body
+        })
+            .then((response) => {
+                if (response.ok) {
+                    // Handle success, e.g., close the modal
+                    onHide();
+                } else {
+                    // Handle errors
+                }
+            })
+            .catch((error) => {
+                console.error('Error sending data to the backend:', error);
+            });
+    };
+
+
+    return (
+        <Modal show={show} onHide={onHide} backdrop="static" keyboard={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Neue Stats hinzufügen</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="createStatsForm.Title">
+                        <Form.Label>Titel</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleChange}
+                            placeholder="Spotify"
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="createStatsForm.Value">
+                        <Form.Label>Wert</Form.Label>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                type="number"
+                                name="value"
+                                value={formData.value}
+                                onChange={handleChange}
+                                placeholder="10000"
+                            />
+                        </InputGroup>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="createStatsForm.Goal">
+                        <Form.Label>Ziel</Form.Label>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                type="number"
+                                name="goal"
+                                value={formData.goal}
+                                onChange={handleChange}
+                                placeholder="100000"
+                            />
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Abbrechen
+                </Button>
+                <Button variant="success" onClick={handleAddStats}>
+                    Hinzufügen
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default CreateStats;
