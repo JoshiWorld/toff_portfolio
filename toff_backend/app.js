d29e6c52a5cc38fc8f438b4d0bc10de496e2ec1a
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 15:34:50 +0200

    added /api/stats route

--- toff_backend/app.js
+++ toff_backend/app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 var liveRouter = require('./routes/live');
 var masterRouter = require('./routes/master');
+var statsRouter = require('./routes/stats');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(cors(corsOptions));
 
 app.use('/api/live', liveRouter);
 app.use('/api/master', masterRouter);
+app.use('/api/stats', statsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
