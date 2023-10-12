0c27f1089c52673614ce52a6dd8fecc11199638c
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-12 02:14:22 +0200

    added image to interface

--- toff_frontend/src/Types/types.ts
+++ toff_frontend/src/Types/types.ts
@@ -1,7 +1,9 @@
 export interface BlogEntryItem {
+    id: number;
     title: string;
     description: string;
     ticketLink?: string;
     imageSource?: string;
     archived: boolean;
+    image?: File;
 }
