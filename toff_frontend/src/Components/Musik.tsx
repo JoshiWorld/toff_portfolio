103d5304c477277e437421558e0261a97666ff49
Joshua Stieber <joshua.stieber.1903@gmail.com>
2023-10-16 09:51:59 +0200

    fixed wrong direction sliding & made elements dynamic

--- toff_frontend/src/Components/Musik.tsx
+++ toff_frontend/src/Components/Musik.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import './Musik.css';
 
 function Musik() {
+    const elementData = [
+        { width: '95%' },
+        { width: '80%' },
+        { width: '90%' },
+    ];
+
     return (
         <div className="charts">
             <main>
                 <section id="tall">
                     <article className="tall">
                         <h1>STATS</h1>
-                        <div>
-                            <p>Test <span></span><span className="tall"></span></p>
-                        </div>
-                        <div>
-                            <p>Test2 <span></span><span className="tall"></span></p>
-                        </div>
-                        <div>
-                            <p>Test3 <span></span><span className="tall"></span></p>
-                        </div>
+                        {elementData.map((data, index) => (
+                            <div key={index}>
+                                <p>Test {data.width} <span></span><span className="tall" style={{ width: data.width, animationDelay: `${index * 0.05}s` }}></span></p>
+                            </div>
+                        ))}
                     </article>
                 </section>
             </main>
