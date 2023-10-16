2181e8c6b3854124b4cd29bff9cf8f8c29259bb6
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-17 00:55:45 +0200

    added insta icon

--- toff_frontend/src/Components/Home.tsx
+++ toff_frontend/src/Components/Home.tsx
@@ -3,6 +3,9 @@ import homeImage from '../Images/home.jpg';
 import Button from 'react-bootstrap/Button';
 import './Home.css';
 import { Spinner } from 'react-bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
     const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -10,6 +13,8 @@ function Home() {
     useEffect(() => {
         setIsLoading(false);
     }, []);
+
+    library.add(faInstagram);
 
 
     const containerStyle: React.CSSProperties = {
@@ -29,24 +34,40 @@ function Home() {
         bottom: 0,
         left: 0,
         width: '100%',
-        height: '10%', // Adjust the height to control the fading area
-        background: 'linear-gradient(transparent, white)', // Create a gradient from transparent to white
-        zIndex: 2, // Ensure the overlay is above the image
+        height: '10%',
+        background: 'linear-gradient(transparent, white)',
+        zIndex: 2,
     };
 
     const buttonStyle: React.CSSProperties = {
         position: 'absolute',
-        top: '50%', // Vertically center the button relative to the picture
-        left: '71%', // Adjust the left position as needed relative to the picture
-        transform: 'translate(-50%, -50%)', // Center the button
-        zIndex: 3, // Ensure the button is above the overlay
+        top: '50%',
+        left: '71%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 3,
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
         textDecoration: 'none',
         padding: 0,
         width: '13vw',
         height: '5vw',
         fontSize: '2vw',
-        whiteSpace: 'nowrap', // Prevent text from breaking into two lines
+        whiteSpace: 'nowrap',
+    };
+
+    const buttonStyleB: React.CSSProperties = {
+        position: 'absolute',
+        top: '60%',
+        left: '71%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 3,
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        border: 'none',
+        textDecoration: 'none',
+        padding: 0,
+        width: '13vw',
+        height: '5vw',
+        fontSize: '2vw',
+        whiteSpace: 'nowrap',
     };
 
     const buttonStyleA: React.CSSProperties = {
@@ -67,9 +88,9 @@ function Home() {
                         <Button className="responsive-button" style={buttonStyle}>JETZT HÖREN</Button>
                     </a>
 
-                    {/*<div className="overlay">*/}
-                    {/*    <a href="https://open.spotify.com/intl-de/artist/35qJJVRDUxWBkMuZhkkCvz?si=v9bIR9JFSeuiIuyeTxt0nQ" target="_blank" rel="noopener noreferrer" ></a>*/}
-                    {/*</div>*/}
+                    <a href="https://www.instagram.com/toff.musik/" target="_blank" rel="noopener noreferrer" style={buttonStyleA} >
+                        <Button className="responsive-button" style={buttonStyleB}><FontAwesomeIcon icon={faInstagram} /></Button>
+                    </a>
                 </div>
             )}
         </>
