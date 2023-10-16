1e7becb493726700b3d644ddb801041d57353bf9
Joshua Stieber <joshua.stieber.1903@gmail.com>
2023-10-16 10:45:06 +0200

    fetching stats data from api done

--- toff_frontend/src/Components/Musik.tsx
+++ toff_frontend/src/Components/Musik.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Musik.css';
+import {StatsItem} from "../Types/types";
+import {API_BASE_URL} from "../config";
 
 function Musik() {
-    const stats = [
-        { id: 1, title: 'Spotify', value: 900, goal: 1000, color: '#01b326' },
-        { id: 2, title: 'Test', value: 1005, goal: 10000, color: '#11eaea' },
-        { id: 3, title: 'Hallo', value: 50000, goal: 60000 },
-    ];
+    const [stats, setStats] = useState<StatsItem[]>([]);
 
-    const statsWithPercentage = stats.map((item) => {
-        const percentage = (item.value / item.goal) * 100;
-        const percentageString = percentage + '%';
-        return { ...item, percentageString };
-    });
+    useEffect(() => {
+        if (stats.length === 0) {
+            // Fetch data from the backend when the component mounts
+            fetch(`${API_BASE_URL}/api/stats`) // Replace with your actual API endpoint
+                .then((response) => response.json())
+                .then((data) => {
+                    // Calculate the percentageString for each item in the data
+                    const statsWithPercentage = data.map((item: StatsItem) => {
+                        // @ts-ignore
+                        const percentage = (item.value / item.goal) * 100;
+                        const percentageString = percentage + '%';
+                        return { ...item, percentageString };
+                    });
+                    // Set the stats state with the calculated data
+                    setStats(statsWithPercentage);
+                })
+                .catch((error) => console.error('Error fetching data:', error));
+        }
+    }, [stats.length]);
 
     return (
         <div className="charts">
@@ -20,7 +32,7 @@ function Musik() {
                 <section id="tall">
                     <article className="tall">
                         <h1>STATS</h1>
-                        {statsWithPercentage.map((data, index) => (
+                        {stats.map((data, index) => (
                             <div key={index}>
                                 <p>{data.title} <span></span><span className="tall" style={{ background: data.color, width: data.percentageString, animationDelay: `${index * 0.05}s` }}></span></p>
                             </div>
