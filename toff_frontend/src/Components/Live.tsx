8aadf83aee396e4b15163bfa19f928a5144bfddc
Joshua Stieber <joshua.stieber.1903@gmail.com>
2023-10-17 09:33:07 +0200

    added new design to Live-Page

--- toff_frontend/src/Components/Live.tsx
+++ toff_frontend/src/Components/Live.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { BlogEntryItem } from '../Types/types';
 import Carousel from 'react-bootstrap/Carousel';
-import { Spinner } from 'react-bootstrap';
+import {Image, Spinner} from 'react-bootstrap';
 import { API_BASE_URL } from '../config';
-import CarouselItemContainer from "./Live/CarouselItemContainer";
 import LiveEmptyPage from "./Live/LiveEmptyPage";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function Live() {
     const [liveAuftritte, setLiveAuftritte] = useState<BlogEntryItem[]>([]);
@@ -23,27 +25,94 @@ function Live() {
         }
     }, [liveAuftritte.length]);
 
+    const BuyTickets: React.CSSProperties = {
+        textDecoration: 'none',
+        color: 'white',
+        border: '3px solid black',
+        borderRadius: '4px',
+        boxShadow: '3px 3px 3px rgba(0, 0, 0, 0.4)',
+    };
+
+    const RemoveLinks: React.CSSProperties = {
+        textDecoration: 'none',
+        color: 'white',
+    }
+
+    const imageStyle: React.CSSProperties = {
+        zIndex: 1,
+        width: "100%",
+        height: "auto",
+        objectFit: "cover",
+        objectPosition: "center",
+    };
+
+    const cardStyle = {
+        border: '0px',
+        boxShadow: '7px 7px 7px rgba(0, 0, 0, 0.4)',
+    };
+
+    const containerStyle = {
+        maxWidth: '300px',
+    };
+
 
     return (
-        <>
+        <div className="container mt-3">
             {isLoading ? (
                 <Spinner animation="grow" />
             ) : (
                 <>
                     {liveAuftritte.length !== 0 ? (
-                        <>
-                            <Carousel>
-                                {liveAuftritte.map((item, index) => (
-                                    <CarouselItemContainer index={index} item={item} />
-                                ))}
-                            </Carousel>
-                        </>
+                        <Carousel style={{ boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.4)', borderRadius: "5px", }}>
+                            {liveAuftritte.map((item, index) => (
+                                <Carousel.Item key={index}>
+                                    <div>
+                                        {item.isVideo ? (
+                                            <video
+                                                src={`${API_BASE_URL}/api/${item.mediaSource}`}
+                                                autoPlay
+                                                muted
+                                                controls={false}
+                                                loop
+                                                style={imageStyle}
+                                            />
+                                        ) : (
+                                            <Image src={`${API_BASE_URL}/api/${item.imageSource}`} alt="Livesource" className="img-fluid"
+                                                   style={imageStyle}/>
+                                        )}
+                                    </div>
+
+                                    <Carousel.Caption>
+                                        <Container style={containerStyle}>
+                                            <Card style={cardStyle}>
+                                                <Card.Header>{item.title}</Card.Header>
+                                                <Card.Body>
+                                                    <Card.Text>
+                                                        {item.description}
+                                                    </Card.Text>
+                                                    <Button variant="dark" style={BuyTickets}>
+                                                        {item.ticketLink ? (
+                                                            <a href={item.ticketLink} target="_blank" rel="noopener noreferrer"
+                                                               style={RemoveLinks}>
+                                                                Tickets
+                                                            </a>
+                                                        ) : (
+                                                            'Freier Eintritt'
+                                                        )}
+                                                    </Button>
+                                                </Card.Body>
+                                            </Card>
+                                        </Container>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>
                     ) : (
                         <LiveEmptyPage />
                     )}
                 </>
             )}
-        </>
+        </div>
     );
 }
 
