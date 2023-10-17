796287168270f3685cecf1f23bbe7b9bf47c3dc8
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-17 17:51:09 +0200

    more fill of the screen

--- toff_frontend/src/Components/MusikPlaylist.tsx
+++ toff_frontend/src/Components/MusikPlaylist.tsx
@@ -4,8 +4,8 @@ import Container from 'react-bootstrap/Container';
 function MusikPlaylist() {
     return(
         <Container>
-            <iframe title="Playlist" style={{ borderRadius: '12px' }}
-                    src="https://open.spotify.com/embed/playlist/2FbQOK7yDfa6Bo0QRUBIgJ?utm_source=generator&theme=0"
+            <iframe title="Artist" style={{ borderRadius: '12px' }}
+                    src="https://open.spotify.com/embed/artist/35qJJVRDUxWBkMuZhkkCvz?utm_source=generator&theme=0"
                     width="100%" height="352" frameBorder="0"
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                     loading="lazy"></iframe>
