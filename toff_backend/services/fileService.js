fd4ab4a3fc30beacb8c599510853c76534da1fab
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-18 18:21:36 +0200

    added file service

---
+++ toff_backend/services/fileService.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function deleteFile(filePath) {
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(`Error deleting file: ${err}`);
+        } else {
+            console.log(`File deleted: ${filePath}`);
+        }
+    });
+}
+
+module.exports = {
+    deleteFile
+}
