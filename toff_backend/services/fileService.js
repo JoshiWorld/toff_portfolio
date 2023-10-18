4dc474ff0d540610992b5b0e03d44279e5ce5cf7
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-10-18 18:23:06 +0200

    finished fileService.js

--- toff_backend/services/fileService.js
+++ toff_backend/services/fileService.js
@@ -1,4 +1,17 @@
 const fs = require('fs');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Specify the directory where uploaded files will be saved
+    },
+    filename: function (req, file, cb) {
+        // Use the current timestamp as a unique file name
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 function deleteFile(filePath) {
     fs.unlink(filePath, (err) => {
@@ -11,5 +24,6 @@ function deleteFile(filePath) {
 }
 
 module.exports = {
-    deleteFile
+    deleteFile,
+    upload
 }
